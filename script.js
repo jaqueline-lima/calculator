6ff4e1ffbbc3644313b2a5191879f49a466c1c40
Jaqueline <jaquelinedlp78@gmail.com>
2022-04-11 11:23:41 -0300

    Added logic to operate from user input.

--- script.js
+++ script.js
@@ -39,13 +39,54 @@ function operate(a, operator, b) {
     return result;
 }
 
-function show(item) {
-    const display = document.querySelector('#display');
+function show(display, item) {
     display.textContent += item;
 }
 
+function getSecondNumber(displayContent) {
+    const operators = ['+', '-', '*', '/'];
+    let index;
+    for (let i = 0; i < operators.length; i++) {
+        for (let j = 0; j < displayContent.length; j++) {
+            if (displayContent[i] === operators[j]) {
+                index = i;
+            }
+        }
+    }
+    const secondNumber = displayContent.slice(index + 1);
+    return secondNumber;
+}
+
+const display = document.querySelector('#display');
+let firstNumber,
+    operator,
+    secondNumber,
+    result,
+    buttonClicked = false; // This variable makes sure that only one operation button is pressed at a time
+
 document.querySelectorAll('#numbers button').forEach(button => {
     button.addEventListener('click', () => {
-        show(button.textContent);
+        show(display, button.textContent);
     })
 });
+
+document.querySelectorAll('#operations button').forEach(button => {
+    button.addEventListener('click', () => {
+        if (!buttonClicked) {
+            firstNumber = display.textContent;
+            show(display, button.textContent);
+            operator = button.textContent;
+            console.log(operator);
+            buttonClicked = true;
+        }
+    })
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    secondNumber = getSecondNumber(display.textContent);
+//    console.log(secondNumber);
+    show(display, '=')
+    result = operate(+firstNumber, operator, +secondNumber);
+    show(display, result);
+});    
